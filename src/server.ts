34d1817473db8f664d994306c259df419fd0a9f8
Andrey Lefkum <andreypedro@gmail.com>
2025-08-11 22:14:59 -0400

    add main package to the project

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "Server is running" });
+});
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
