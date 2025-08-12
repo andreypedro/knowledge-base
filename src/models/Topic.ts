f7e6d0409c91b420bd227fb4e95e2574f305209f
Andrey Lefkum <andreypedro@gmail.com>
2025-08-12 14:05:40 -0400

    feat: create topic model and base entity

---
+++ src/models/Topic.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from "../core/BaseEntity";
+import { ITopics } from "./ITopic";
+
+export class Topic extends BaseEntity implements ITopics {
+  public name: string;
+  public content: string;
+  public version: number;
+  public parentTopicId?: string | null = null;
+
+  constructor(
+    name: string,
+    content: string,
+    version = 1,
+    parentTopicId?: string | null,
+    id?: string,
+    updatedAt?: Date
+  ) {
+    super(id);
+    this.name = name;
+    this.content = content;
+    this.version = version;
+    this.parentTopicId = parentTopicId;
+    this.updatedAt = updatedAt ?? new Date();
+  }
+}
