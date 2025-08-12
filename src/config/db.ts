2990e11adf6d8ed556e3a2f1632571761106f1df
Andrey Lefkum <andreypedro@gmail.com>
2025-08-12 13:18:56 -0400

    feat: add lowdb and create structure to use json as database simulation.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,8 @@
 import low from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
+import { ITopics } from "../models/ITopic";
+import { IResources } from "../models/IResource";
+import { IUsers } from "../models/IUser";
 
 type Schema = {
   topics: ITopics[];
